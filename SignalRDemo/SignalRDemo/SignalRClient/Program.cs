584b6da73b03a71a9bbba8349745ee36e3172a26
Ganesh <ganeshkumar.r@knila.com>
2020-02-12 12:58:39 +0530

    Initial Commit

---
+++ SignalRDemo/SignalRDemo/SignalRClient/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNet.SignalR.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalRClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IHubProxy _hub;
+            string url = @"http://localhost:8080/";
+            var connection = new HubConnection(url);
+            _hub = connection.CreateHubProxy("TestHub");
+            connection.Start().Wait();
+
+            _hub.On("ReceiveLength", x => Console.WriteLine(x));
+
+            string line = null;
+            while ((line = System.Console.ReadLine()) != null)
+            {
+                _hub.Invoke("DetermineLength", line).Wait();
+            }
+
+            Console.Read();
+        }
+    }
+}
---
+++ SignalRDemo/SignalRDemo/SignalRDemo/TestHub.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalRHub
+{
+    [HubName("TestHub")]
+    public class TestHub : Hub
+    {
+        public void DetermineLength(string message)
+        {
+            Console.WriteLine(message);
+
+            string newMessage = string.Format(@"{0} has a length of: {1}", message, message.Length);
+            Clients.All.ReceiveLength(newMessage);
+        }
+    }
+}
